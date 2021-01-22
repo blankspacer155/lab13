1b0dad2f1fcd81d5bdb8622073c5c1f805eaa53a
blankspacer155 <monnoi.92@gmail.com>
2021-01-22 15:42:37 +0700

    13-1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i=1;i<N;i++)
+	{
+		int j=i;
+		while(d[j]>d[j-1] and j>0)
+		{
+			T temp=d[j];
+			d[j]=d[j-1];
+			d[j-1]=temp;
+			j--;
+		}
+		cout<<"Pass "<<i<<":";
+		for(int i2=0;i2<N;i2++)
+		{
+			cout<<d[i2]<<" ";
+		}
+		cout<<endl;
+	}
 }
 
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	int a[10] = {10,9,8,7,6,5,4,3,2,1};
 	cout << "Input Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
